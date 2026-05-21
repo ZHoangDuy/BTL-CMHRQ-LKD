d7e0c356547890fc639e5eeee33f27786f9b476d
Lê Khương Duy <103999244+ZHoangDuy@users.noreply.github.com>
2026-05-21 16:51:31 +0700

    Delete aideom_dashboard (1) directory

--- aideom_dashboard (1)/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-# modules/__init__.py
-from .m1_forecast import run_m1
-from .m2_readiness import run_m2
-from .m3_optimization import run_m3
-from .m4_labor import run_m4
-from .m5_risk import run_m5
